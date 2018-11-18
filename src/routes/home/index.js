3d9e7c5b7016d9e17736d0ddbbd9ef90dc710ec5
Jackson Lenhart <jacksonlenhart@gmail.com>
2018-11-18 01:03:49 -0500

    Get working with API and fix large bug with logic

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,48 +8,96 @@ class Home extends Component {
 	state = {
 		gameResult: null,
 		hangmanString: '',
-		guessed: []
+		guessed: [],
+		wrongGuesses: 0,
+		fullGuess: ''
 	};
 
 	componentDidMount() {
 		// make request to service for hangman string...
-		setTimeout(() => {
-			this.setState({ hangmanString: 'test' });
-		}, 2000);
-	}
-
-	guess(letter) {
-		if (this.state.gameResult === 'failure') {
-			return;
-		}
-
-		if (this.state.gameResult === 'success') {
-			return;
-		}
-
-		this.setState(prevState => {
-			const guessed = prevState.guessed.concat(letter);
-			if (prevState.hangmanString.split('')
-				.every(c => guessed.includes(c))
-			) {
-				return {
-					guessed,
-					gameResult: 'success'
-				};
-			}
-			else if (guessed.length > 6) {
-				return {
-					guessed,
-					gameResult: 'failure'
-				};
-			}
-			else {
-				return { guessed };
-			}
+		fetch('http://localhost:6969/')
+		.then(res => res.text())
+		.then(str => {
+			this.setState({
+				hangmanString: str
+			});
 		});
 	}
 
-	render({}, { hangmanString, gameResult, guessed }) {
+	fullGuess() {
+		const guess = this.state.fullGuess;
+		if (guess === this.state.hangmanString) {
+			this.setState({
+				gameResult: 'success',
+			});
+		}
+		else if (this.state.wrongGuesses >= 7) {
+			this.setState(prevState => ({
+				gameResult: 'failure',
+				wrongGuesses: prevState.wrongGuesses + 1
+			}));
+		}
+		else {
+			this.setState(prevState => ({
+				wrongGuesses: prevState.wrongGuesses + 1
+			}));
+		}
+	}
+
+	guess(letter) {
+		const { hangmanString, gameResult, wrongGuesses } = this.state;
+
+		if (gameResult === 'failure') {
+			return;
+		}
+
+		if (gameResult === 'success') {
+			return;
+		}
+
+		const letters = hangmanString.split('')
+			.filter(c => /[A-Z0-9]/i.test(c))
+
+		const uniqueLetters = [];
+		for (const c of letters) {
+			const l = c.toLowerCase();
+			if (!uniqueLetters.includes(l)) {
+				uniqueLetters.push(l);
+			}
+		}
+
+		if (uniqueLetters.includes(letter.toLowerCase())
+			&& !this.state.guessed.includes(letter.toLowerCase())
+		) {
+			this.setState(prevState => {
+				const guessed = prevState.guessed.concat(letter);
+				return {
+					guessed,
+					gameResult: guessed.length >= uniqueLetters.length
+						? 'success' : prevState.gameResult
+				};
+			});
+		}
+		else if (wrongGuesses >= 7) {
+			this.setState(prevState => ({
+				gameResult: 'failure',
+				wrongGuesses: prevState.wrongGuesses + 1
+			}));
+		}
+		else {
+			this.setState(prevState => ({
+				wrongGuesses: prevState.wrongGuesses + 1
+			}));
+		}
+	}
+
+	handleChange(e) {
+		this.setState({
+			fullGuess: e.target.value
+		});
+	}
+
+	render({}, { hangmanString, gameResult, guessed, wrongGuesses }) {
 		return (
 			<div class={style.home}>
 				{
@@ -62,7 +110,9 @@ class Home extends Component {
 									hangmanString.split('').map(c => (
 										<span class={style.blank}>
 											{
-												guessed.includes(c) ? c : '_'
+												gameResult === 'success' ? c : (
+													guessed.includes(c.toLowerCase()) ? c : '_'
+												)
 											}
 										</span>
 									))
@@ -70,11 +120,26 @@ class Home extends Component {
 							</div>
 							<div class={style.lettersContainer}>
 								{
-									letters.split('').map(l => (
+									letters.split('')
+									.filter(l => !guessed.includes(l))
+									.map(l => (
 										<span class={style.btnContainer}>
 											<button onClick={() => this.guess(l)} type="button">{l}</button>
 										</span>
 									))
+								}
+							</div>
+							<div>
+								{
+									!gameResult ? (
+										<div>
+											<textarea onChange={this.handleChange.bind(this)} />
+											<button onClick={this.fullGuess.bind(this)} type="button">Full Guess</button>
+										</div>
+									) : ''
+								}
+								{
+									<h2>{wrongGuesses} wrong guesses</h2>
 								}
 							</div>
 						</div>
